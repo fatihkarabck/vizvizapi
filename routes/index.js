5ea8d56d140c087f4e749dc1af1079707f316848
fatihkarabck34 <fatihkarabck@gmail.com>
2020-03-18 15:55:35 +0300

    5
    5

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,11 @@ router.post('/', function(req, res, next) {
 
     sql.query("INSERT INTO users SET name='"+req.param('name')+"',email='"+req.param('email')+"',tel='"+req.param('tel')+"',pass='"+req.param('pass')+"',plaka='"+req.param('plaka')+"',type='"+req.param('type')+"',ref='"+req.param('ref')+"'");
 
-  sql.query("SELECT userid FROM users WHERE tel='"+req.param('tel')+"',email='"+req.param("email")+"'",function (err,rows) {
+  sql.query("SELECT userid FROM users WHERE tel='"+req.param('tel')+"' AND email='"+req.param("email")+"'",function (err,rows) {
     if (err) throw err;
     console.log(rows);
 
-    res.send(rows);
+    res.send(rows[0]);
   });
 
 
